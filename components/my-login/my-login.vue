bb274b5db4516e64fac49d54b3c7cd9837d697ce
MoreTX <zhaoclouwave@qq.com>
2022-05-11 14:05:28 +0800

    小程序代码创建

---
+++ components/my-login/my-login.vue
@@ -0,0 +1,20 @@
+<template>
+	<view>
+		登录组件
+	</view>
+</template>
+
+<script>
+	export default {
+		name:"my-login",
+		data() {
+			return {
+				
+			};
+		}
+	}
+</script>
+
+<style lang="scss">
+
+</style>
